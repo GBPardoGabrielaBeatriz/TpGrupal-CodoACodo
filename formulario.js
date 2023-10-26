17bb455abba8a6efcff3d1ed51b9ebdac287fcc5
ji-berti <104868738+ji-berti@users.noreply.github.com>
2023-10-26 01:32:19 -0300

    Add files via upload

---
+++ formulario.js
@@ -0,0 +1,63 @@
+document.getElementById('reservationForm').addEventListener('submit', async function(event) {
+    event.preventDefault();
+
+    const fullName = document.getElementById('fullName').value.trim();
+    const email = document.getElementById('email').value.trim();
+    const phone = document.getElementById('phone').value.trim();
+    const date = document.getElementById('date').value;
+    const people = document.getElementById('people').value;
+    const message = document.getElementById('message').value.trim();
+
+    let sucursalSelected = document.querySelector('input[name="sucursal"]:checked');
+    const sucursal = sucursalSelected ? sucursalSelected.value : null;
+
+    if (validateForm(fullName, email, phone, date, people, message, sucursal)) {
+        const formData = new FormData(event.target);
+
+        const response = await fetch(event.target.action, {
+            method: 'POST',
+            body: formData,
+            headers: {
+                'Accept': 'application/json'
+            }
+        });
+
+        if (response.ok) {
+            alert('Reserva realizada con éxito');
+            event.target.reset();  
+        } else {
+            alert('Hubo un error al enviar el formulario.');
+        }
+    } else {
+        alert('Por favor, completa todos los campos correctamente.');
+    }
+
+    // Imprimir los valores en consola
+    console.log("Nombre y Apellido:", fullName);
+    console.log("Correo Electrónico:", email);
+    console.log("Número de Teléfono:", phone);
+    console.log("Fecha de Reserva:", date);
+    console.log("Cantidad de Comensales:", people);
+    console.log("Sucursal:", sucursal);
+    console.log("Restricciones alimenticias:", message);
+});
+
+function validateForm(fullName, email, phone, date, people, message, sucursal) {
+    if (!fullName || !email || !phone || !date || !people || !sucursal) {
+        return false;
+    }
+
+    let emailPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
+    if (!email.match(emailPattern)) {
+        alert('Formato de correo electrónico no válido.');
+        return false;
+    }
+
+    let phonePattern = /^[0-9]{10}$/;
+    if (!phone.match(phonePattern)) {
+        alert('Formato de número de teléfono no válido. Debe contener exactamente 10 dígitos.');
+        return false;
+    }
+
+    return true;
+}
